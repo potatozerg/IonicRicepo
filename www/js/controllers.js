d6c60e7126b24a1e2fffe4d846aac7bcc626ed7d
god830 <yukun_liu2009@hotmail.com>
2017-06-27 16:29:47 -0400

    second day working on two way binding

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,7 @@ var app = angular.module('starter.controllers', []);
 
 app.controller('mainCtrl',function($scope){
 
+	$scope.showGeoJSON = [];
 	$scope.testPath = [
 					    [
 					      
@@ -25,12 +26,50 @@ app.controller('mainCtrl',function($scope){
 					    ]
 					];
 			
-	$scope.testPathString = JSON.stringify($scope.testPath);
+	//$scope.testPathString = JSON.stringify($scope.testPath);
+
+
+	$scope.testPath1 = [
+			            [
+			              
+			              40.746736760718505,-73.98948669433594
+			            ],
+			            [
+			              
+			              40.706148461723764,-74.01369094848633
+			            ],
+			            [
+			              
+			              40.7375024965684,-73.98056030273438
+			            ],
+			            [
+			              
+			              40.746736760718505,-73.98948669433594
+			            ]
+			        ];
 	$scope.areas = [
 		{
-			path:$scope.testPathString,
+			path:$scope.testPath,
 			deleted:false
 		}
+		// ,
+		// {
+		// 	path:$scope.testPath1,
+		// 	deleted:false
+		// }
 	];
+	localStorage.setItem('storedAreas', JSON.stringify($scope.areas));
+
+	$scope.add = function(){
+		$scope.areas.push({path:"",deleted:false});
+		localStorage.setItem('storedAreas', JSON.stringify($scope.areas));
+		$scope.areas = localStorage.getItem('storedAreas');
+	}
+
+	$scope.textAreas = [];
+	$scope.stringifyPath = function(index){
+		$scope.textAreas[index] = JSON.stringify($scope.areas[index]); 
+
+	}
 	
 });
